27b57918b147cdf55d64173e8adc1e7c84780db8
raybuhr <rebuhr@gmail.com>
2018-12-01 20:11:49 -0600

    day1 and ignore vscode

--- 2018/1/day1.py
+++ 2018/1/day1.py
@@ -15,28 +15,22 @@ def update_frequency(change: str, current_frequency: int) -> int:
 frequency = 0
 frequency_map = {frequency: 1}
 not_found_repeat_frequency = True
-frequencies_seen = [0]
-
-for change in changes:
-    frequency = update_frequency(change, frequency)
-    if frequency_map.get(frequency) is None:
-        frequency_map[frequency] = 1
-    else:
-        frequency_map[frequency] += 1
-    if frequency_map[frequency] == 2 and not_found_repeat_frequency:
-        print(frequency)
-        not_found_repeat_frequency = False
-
-print('Part One:', frequency)
 
 while not_found_repeat_frequency:
+    len_input = len(changes)
+    iterations = 0
     for change in cycle(changes):
         frequency = update_frequency(change, frequency)
+        iterations += 1
         if frequency_map.get(frequency) is None:
             frequency_map[frequency] = 1
         else:
             frequency_map[frequency] += 1
+        if iterations == len_input:
+            print('Part One:', frequency)
         if frequency_map[frequency] == 2 and not_found_repeat_frequency:
             print('Part Two:', frequency)
             not_found_repeat_frequency = False
             break
+
+print('Iterations:', iterations)
